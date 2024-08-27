492ea3fc0c441eef6fbedc086aacc730ad2d6345
Nekumo <nekumo.ac@gmail.com>
2024-08-27 21:46:30 +0200

    add verif Champ formulaire d'inscription

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -1,31 +1,98 @@
 //implémenter le JS de ma page
 
 const inputNom = document.getElementById("NomInput");
-const inputPreNom = document.getElementById("PreNomInput");
+const inputPreNom = document.getElementById("PrenomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
-const inputPasswordInput = document.getElementById("ValidatePasswordInput");
+const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+const btnValidation = document.getElementById("btn-validation-inscription");
 
 inputNom.addEventListener("keyup", validateForm);
 inputPreNom.addEventListener("keyup", validateForm);
 inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
-inputPasswordInput.addEventListener("keyup", validateForm);
+inputValidationPassword.addEventListener("keyup", validateForm);
+
+//fonction permettant de valider tout le formulaire
 
 function validateForm(){
-    validateRequired(inputNom);
-    validateRequired(inputPreNom);
+    const nomOK = validateRequired(inputNom);
+    const prenomOK = validateRequired(inputPreNom);
+    const mailOK = validateMail(inputMail);
+    const passwordOK = validatePassword(inputPassword);
+    const passwordConfirmOk = ValidateConfirmationPassword(inputPassword, inputValidationPassword);
+
+    if(nomOK && prenomOK && mailOK && passwordOK && passwordConfirmOk){
+        btnValidation.disabled =false;
+    }
+    else{
+        btnValidation.disabled =true;
+    }
 }
 
-function validateRequired(input){
-    if(input.value != ''){
+function validateMail(input){
+    //Définir mon regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    if(mailUser.match(emailRegex)){
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
+        return true;
         //Si la valeur est différente de rien : c'est ok
     }
     else{
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
+        return false;
+            //c'est pas ok
+            //c'est pas ok
+    }
+}
+
+function ValidateConfirmationPassword(inputPwd, inputConfirmPwd){
+    if(inputPwd.value == inputConfirmPwd.value){
+        inputConfirmPwd.classList.add("is-valid");
+        inputConfirmPwd.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        inputConfirmPwd.classList.add("is-invalid");
+        inputConfirmPwd.classList.remove("is-valid");
+        return false;
+    }
+}
+
+function validatePassword(input){
+    //Définir mon regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+        //Si la valeur est différente de rien : c'est ok
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+            //c'est pas ok
+            //c'est pas ok
+    }
+}
+
+
+function validateRequired(input){
+    if(input.value != ''){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+        //Si la valeur est différente de rien : c'est ok
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
             //c'est pas ok
     }
 }
